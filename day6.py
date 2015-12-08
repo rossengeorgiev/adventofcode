d97a342dac0b9acaebc429dd485590eeeea552df
Rossen Georgiev <rossen@rgp.io>
2015-12-08 04:21:58 +0000

    updated day6 with part2

--- day6.py
+++ day6.py
@@ -10,14 +10,18 @@ def listen_to_santa(line):
             pos = i*1000 + j
             if cmd == 'toggle':
                 lights[pos] = 1 if lights[pos] == 0 else 0
+                lights2[pos] += 2
             else:
                 lights[pos] = 1 if state == 'on' else 0
+                lights2[pos] += 1 if state == 'on' else -1
+                lights2[pos] = max(0, lights2[pos])
 
 lights = [0] * 1000000
+lights2 = [0] * 1000000
 
 print("Input (empty line to finish): ")
 
 map(listen_to_santa, iter(raw_input, ''))
-answer = lights.count(1)
 
-print("Answer: %d" % answer)
+print("Answer (part1): %d" % sum(lights))
+print("Answer (part2): %d" % sum(lights2))
