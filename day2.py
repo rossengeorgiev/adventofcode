582b2fc87437466a7e78d773097efcfe922522c1
Rossen Georgiev <rossen@rgp.io>
2015-12-08 05:58:26 +0000

    updated day2 with part2

--- day2.py
+++ day2.py
@@ -1,13 +1,22 @@
 from __future__ import print_function
 
-def paper_size(line):
+def parse_dimensions(line):
     if not line: return 0
-    l, w, h = map(int, line.split('x'))
+    return map(int, line.split('x'))
+
+def paper_size(d):
+    l, w, h = d
     sides = [l*w, w*h, h*l]
     return sum(map(lambda x: x*2, sides)) + min(sides)
 
+def ribbon_length(d):
+    length = reduce(lambda a,b: a*b, d)
+    d.pop(d.index(max(d)))
+    return length + sum(d*2)
+
 print("Input (empty line to finish): ")
 
-answer = sum(map(paper_size, iter(raw_input, '')))
+data = map(parse_dimensions, iter(raw_input, ''))
 
-print("Answer: %d" % answer)
+print("Answer (part1): %d" % sum(map(paper_size, data)))
+print("Answer (part2): %d" % sum(map(ribbon_length, data)))
