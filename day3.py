6014be2ad0949bc2ced19bd39db2d4439e2c7d6d
Rossen Georgiev <rossen@rgp.io>
2015-12-08 05:43:16 +0000

    updated day3 with part2

--- day3.py
+++ day3.py
@@ -3,17 +3,22 @@ from __future__ import print_function
 a = raw_input("Input: ")
 
 direction_map = {
-    '^': lambda: (x, y+1),
-    'v': lambda: (x, y-1),
-    '>': lambda: (x+1, y),
-    '<': lambda: (x-1, y),
+    '^': lambda x, y: (x, y+1),
+    'v': lambda x, y: (x, y-1),
+    '>': lambda x, y: (x+1, y),
+    '<': lambda x, y: (x-1, y),
 }
 
-x = y = 0
-house_visited = [(x, y)]
+def follow_route(n_santas):
+    santas = dict(enumerate([(0, 0)]*n_santas))
+    houses_visited = [(0, 0)] * n_santas
 
-for c in list(a):
-    x, y = direction_map[c]()
-    house_visited.append((x, y))
+    for i, c in enumerate(list(a)):
+        xy = santas[i % n_santas]
+        xy = santas[i % n_santas] = direction_map[c](*xy)
+        houses_visited.append(xy)
 
-print("Answer: %d" % len(set(house_visited)))
+    return len(set(houses_visited))
+
+print("Answer (part1): %d" % follow_route(1))
+print("Answer (part2): %d" % follow_route(2))
