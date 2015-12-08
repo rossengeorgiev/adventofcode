6a5e7d0e69d71cbd6ee8c1273a0826ae6bdae550
Rossen Georgiev <rossen@rgp.io>
2015-12-08 06:07:12 +0000

    updated day1 with part2

--- day1.py
+++ day1.py
@@ -1,5 +1,12 @@
 from __future__ import print_function
 
-a = raw_input("Input: ")
+floor = 0
+basement = None
+for index, c in enumerate(list(raw_input("Input: "))):
+    floor += 1 if c == '(' else -1
 
-print("Answer: %d" % ( a.count('(') - a.count(')') ))
+    if floor == -1 and basement is None:
+        basement = index + 1
+
+print("Answer (part1): %d" % floor)
+print("Answer (part2): %d" % basement)
