bab5e0f23cdfbb0947ebb9aad94291514e0e184e
Rossen Georgiev <rossen@rgp.io>
2015-12-12 21:21:14 +0000

    update day12 to regex only

--- day12.py
+++ day12.py
@@ -1,26 +1,23 @@
 from __future__ import print_function
 import re
-import json
+
+def find_and_sum_ints(text):
+    return sum(map(int, re.findall(r'-?\d+', text)))
+
+def sub_reduce(match):
+    text = match.group()
+    if ':"red"' not in text:
+        return str(find_and_sum_ints(text))
+    return '0'
+
+def no_red_sum(data):
+    while not data.isdigit():
+        data = re.sub(r'{[^{}]+}', sub_reduce, data)
+    return int(data)
 
 print("Input (empty line to finish): ")
 
 data = ''.join(list(iter(raw_input, '')))
 
-answer = sum(map(int, re.findall(r'-?\d+', data)))
-
-print("Answer (part1): %s" % answer)
-
-data = json.loads(data)
-
-def redsum(part):
-    if isinstance(part, int):
-        return part
-    if isinstance(part, (dict, list)):
-        if isinstance(part, dict):
-            part = part.values()
-            if 'red' in part:
-                return 0
-        return sum(map(redsum, part))
-    return 0
-
-print("Answer (part2): %s" % redsum(data))
+print("Answer (part1): %s" % find_and_sum_ints(data))
+print("Answer (part2): %s" % no_red_sum(data))
