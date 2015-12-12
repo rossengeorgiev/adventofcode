9892a4e71157af2d28d479199bdc36faccb64d09
Rossen Georgiev <rossen@rgp.io>
2015-12-12 03:50:02 +0000

    shorter code for day2

--- day2.py
+++ day2.py
@@ -1,18 +1,16 @@
 from __future__ import print_function
 
 def parse_dimensions(line):
-    if not line: return 0
     return map(int, line.split('x'))
 
 def paper_size(d):
     l, w, h = d
     sides = [l*w, w*h, h*l]
-    return sum(map(lambda x: x*2, sides)) + min(sides)
+    return sum(sides * 2) + min(sides)
 
 def ribbon_length(d):
-    length = reduce(lambda a,b: a*b, d)
-    d.pop(d.index(max(d)))
-    return length + sum(d*2)
+    length = reduce(lambda a, b: a*b, d)
+    return length + sum(sorted(d)[:-1] * 2)
 
 print("Input (empty line to finish): ")
 
