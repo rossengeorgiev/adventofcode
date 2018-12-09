9bc00e1a6698a374673e602913fb831010ae7c5e
Rossen Georgiev <rossen@rgp.io>
2018-12-09 17:42:51 +0000

    reimplemented day9 with deque

--- day9.py
+++ day9.py
@@ -2,35 +2,25 @@ from aoc import aoc_web_session
 web = aoc_web_session()
 
 import re
-from itertools import cycle
-from blist import blist
+from collections import deque, Counter
 
 data = web.get('https://adventofcode.com/2018/day/9/input').text
 n_players, n_marbles = [int(x) for x in re.search("(\d+) players; last marble is worth (\d+) points", data).groups()]
 
-
 def simulate_game(n_players, n_marbles):
-    scores = [0] * n_players
-    gameboard = blist([0])
-    center = 0
+    scores = Counter()
+    circle = deque([0])
 
     for i in range(1, n_marbles+1):
         if i % 23 == 0:
-            center = center - 7
-            scores[(i - 1) % n_players] += i + gameboard.pop(center)
+            circle.rotate(7)
+            scores[i % n_players] += i + circle.pop()
+            circle.rotate(-1)
         else:
-            center = (center + 2) % (len(gameboard) + 1)
-            center = center if center else center+1
-            gameboard.insert(center, i)
+            circle.rotate(-1)
+            circle.append(i)
 
-    #   print("[%d] %s" % (p+1, repr(gameboard)))
+    return scores.most_common(1)[0][1]
 
-    score = max(scores)
-    winner = scores.index(score) + 1
-
-    return winner, score
-
-print("Part 1: Player %s wins; score %d" % simulate_game(n_players, n_marbles))
-print("Part 2: Player %s wins; score %d" % simulate_game(n_players, n_marbles*100))
-
-
+print("Part 1: Winner score %d" % simulate_game(n_players, n_marbles))
+print("Part 2: Winner score %d" % simulate_game(n_players, n_marbles*100))
